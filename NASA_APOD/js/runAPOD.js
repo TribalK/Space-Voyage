9dee2c52e6a6f2818759434a83a862c6c3bd9240
Derek Drackley <derekdrackley@gmail.com>
2022-04-12 21:28:25 -0500

    Added logic to halt fetch request if user manually keyed in an invalid date

--- NASA_APOD/js/runAPOD.js
+++ NASA_APOD/js/runAPOD.js
@@ -1,5 +1,6 @@
 let dateToday = new Date();
 const dateObj = document.querySelector('#dateSelect');
+const dateMin = '1995-06-01';
 
 let key = config.NASA_API_KEY;
 
@@ -12,27 +13,32 @@ getInitialDateImage();
 dateObj.addEventListener('change', fetchImage);
 
 function fetchImage() {
-  url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${dateObj.value}`;
+  resetAll();
+  if(dateObj.value >= dateMin && dateObj.value <= dateToday) {
+    url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${dateObj.value}`;
 
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      document.querySelector('h2').innerText = data.title;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        document.querySelector('h2').innerText = data.title;
 
-      if(data.media_type === 'image') {
-        document.querySelector('img').src = data.url;
-        document.querySelector('img').alt = data.title;
-        document.querySelector('iframe').src = "";
-      } else if(data.media_type === 'video') {
-        document.querySelector('iframe').src = data.url;
-        document.querySelector('img').src = "";
-        document.querySelector('img').alt = "";
-      }
+        if(data.media_type === 'image') {
+          document.querySelector('img').src = data.url;
+          document.querySelector('img').alt = data.title;
+          document.querySelector('iframe').src = "";
+        } else if(data.media_type === 'video') {
+          document.querySelector('iframe').src = data.url;
+          document.querySelector('img').src = "";
+          document.querySelector('img').alt = "";
+        }
 
-      document.querySelector('figcaption').innerText = data.copyright + " " + data.date;
-      document.querySelector('p').innerText = data.explanation;
-    })
-    .catch(err => console.log(err));
+        document.querySelector('figcaption').innerText = data.copyright + " " + data.date;
+        document.querySelector('p').innerText = data.explanation;
+      })
+      .catch(err => console.log(err));
+    } else {
+      document.querySelector('h2').innerText = dateObj.validationMessage;
+    }
 }
 
 function getInitialDateImage() {
@@ -43,6 +49,16 @@ function getInitialDateImage() {
   dateToday = `${yyyy}-${mm}-${dd}`;
 
   dateObj.value = dateToday;
+  dateObj.min = dateMin;
   dateObj.max = dateToday;
   fetchImage();
+}
+
+function resetAll() {
+  document.querySelector('h2').innerText = "";
+  document.querySelector('img').src = "";
+  document.querySelector('img').alt = "";
+  document.querySelector('iframe').src = "";
+  document.querySelector('figcaption').innerText = "";
+  document.querySelector('p').innerText = "";
 }
